5b97cd39f3dd7e0fb0908492485b9f7b0ee82049
Atlar <tuxoehebo@yandex.ru>
2019-07-02 08:56:51 +0300

    Switch mongo import back to mongodb.org

--- database_agent/mongodb.go
+++ database_agent/mongodb.go
@@ -7,9 +7,9 @@ import (
 	"time"
 
 	//mongodb driver
-	"github.com/mongodb/mongo-go-driver/bson"
-	"github.com/mongodb/mongo-go-driver/mongo"
-	"github.com/mongodb/mongo-go-driver/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/options"
 )
 
 //init
